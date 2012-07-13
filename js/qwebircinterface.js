6b78fda81f31a0cded0418470abdfc669bc93189
Daniel STAN <daniel.stan@crans.org>
2012-07-13 16:55:58 +0200

    small clean-up

--- js/qwebircinterface.js
+++ js/qwebircinterface.js
@@ -13,7 +13,7 @@ qwebirc.ui.Interface = new Class({
   options: {
     initialNickname: "qwebirc" + Math.ceil(Math.random() * 100000),
     initialChannels: "",
-    suggestedChannels: [['#bla','hey !']],
+    suggestedChannels: {},
     networkName: "ExampleNetwork",
     networkServices: [],
     loginRegex: null,
